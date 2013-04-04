c086bc3e04e8a355cd64602db5b0dc965e5625df
mugamba <mate.sosa@gmail.com>
2013-04-03 23:13:44 -0700

    DivingCompetitionData

---
+++ DivingCompetition.Data/Drzava/DrzavaRepository.cs
@@ -0,0 +1,12 @@
+﻿using DivingCompetition.Model;
+using NHibernate;
+
+namespace DivingCompetition.Data
+{
+
+    public class DrzavaRepository : Data.Repository<Drzava>, IDrzavaRepository
+    {
+        public DrzavaRepository(ISessionFactory sessionFactory)
+            : base(sessionFactory){}
+    }
+}
---
+++ DivingCompetition.Data/Mjesto/MjestoRepository.cs
@@ -0,0 +1,12 @@
+﻿using DivingCompetition.Model;
+using NHibernate;
+
+namespace DivingCompetition.Data
+{
+
+    public class MjestoRepository : Repository<Mjesto>, IMjestoRepository
+    {
+        public MjestoRepository(ISessionFactory sessionFactory)
+            : base(sessionFactory){}
+    }
+}
